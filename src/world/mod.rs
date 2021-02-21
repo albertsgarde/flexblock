e7395cc196348bd4e74b54bd98d929fea06ff0ee
Albert Garde <albertsgarde@gmail.com>
2021-02-21 16:25:10 +0100

    :sparkles: Implement Display trait for Location

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -4,6 +4,7 @@ pub mod voxel;
 
 /// Defines a integer location in the world.
 /// Specifies a voxel.
+#[derive(Clone, Copy, Debug)]
 pub struct Location {
     x: i32,
     y: i32,
@@ -11,7 +12,14 @@ pub struct Location {
 }
 
 impl Location {
+    /// Creates a new Location from specified coordinates.
     pub fn new(x: i32, y: i32, z: i32) -> Location {
         Location { x, y, z }
     }
+}
+
+impl std::fmt::Display for Location {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    } 
 }
