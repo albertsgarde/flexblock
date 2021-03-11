563f96388dac2ac96c5503d662d08293cf9bf3a4
Albert Garde <albertsgarde@gmail.com>
2021-03-11 16:39:25 +0100

    :sparkles: Add messages in the three main threads

--- src/game/logic.rs
+++ src/game/logic.rs
@@ -2,5 +2,7 @@ use crate::channels::*;
 use tokio::task::{self, JoinHandle};
 
 pub fn start_logic_thread(_: WindowToLogicReceiver, _: LogicToPackingSender) -> JoinHandle<()> {
-    task::spawn(async {})
+    task::spawn(async {
+        println!("Running game logic!");
+    })
 }
--- src/graphics/packing.rs
+++ src/graphics/packing.rs
@@ -2,5 +2,7 @@ use crate::channels::*;
 use tokio::task::{self, JoinHandle};
 
 pub fn start_packing_thread(_: LogicToPackingReceiver, _: PackingToWindowSender) -> JoinHandle<()> {
-    task::spawn(async {})
+    task::spawn(async {
+        println!("Ready to pack graphics bindings!");
+    })
 }
