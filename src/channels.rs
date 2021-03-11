df7163006c6b00498434bb2705509f22dc0e1179
Albert Garde <albertsgarde@gmail.com>
2021-03-11 15:42:21 +0100

    :sparkles: Make channel struct members public

--- src/channels.rs
+++ src/channels.rs
@@ -7,27 +7,27 @@ use tokio::sync::mpsc;
 pub struct Update;
 
 pub struct WindowToLogicSender {
-    channel_sender: mpsc::Sender<InputEvent>,
+    pub channel_sender: mpsc::Sender<InputEvent>,
 }
 
 pub struct WindowToLogicReceiver {
-    channel_receiver: mpsc::Receiver<InputEvent>,
+    pub channel_receiver: mpsc::Receiver<InputEvent>,
 }
 
 pub struct LogicToPackingSender {
-    channel_sender: mpsc::Sender<Update>,
-    graphics_state_model: Mutex<GraphicsStateModel>,
+    pub channel_sender: mpsc::Sender<Update>,
+    pub graphics_state_model: Mutex<GraphicsStateModel>,
 }
 
 pub struct LogicToPackingReceiver {
-    channel_receiver: mpsc::Receiver<Update>,
-    graphics_state_model: Mutex<GraphicsStateModel>,
+    pub channel_receiver: mpsc::Receiver<Update>,
+    pub graphics_state_model: Mutex<GraphicsStateModel>,
 }
 
 pub struct PackingToWindowSender {
-    bindings: Mutex<Bindings>,
+    pub bindings: Mutex<Bindings>,
 }
 
 pub struct PackingToWindowReceiver {
-    bindings: Mutex<Bindings>,
+    pub bindings: Mutex<Bindings>,
 }
