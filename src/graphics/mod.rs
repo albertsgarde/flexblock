6cd05a9f6f05944e6485f24a79e0b5463c70951f
Albert Garde <albertsgarde@gmail.com>
2021-03-11 16:31:16 +0100

    :sparkles: Start shells of methods to start graphics threads

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,2 +1,5 @@
 mod bindings;
 pub use bindings::Bindings;
+
+mod window;
+pub use window::start_window_thread;
---
+++ src/graphics/packing.rs
@@ -0,0 +1,8 @@
+use crate::channels::*;
+use tokio::task::{self, JoinHandle};
+
+pub fn start_packing_thread(_: LogicToPackingReceiver, _: PackingToWindowSender) -> JoinHandle<()> {
+    task::spawn(async {
+        
+    })
+}
