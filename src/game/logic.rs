43babc7086c8b148b20d5479570dc659542bf554
Albert Garde <albertsgarde@gmail.com>
2021-03-12 21:21:42 +0100

    :sparkles: Implement basic logic loop

--- src/game/logic.rs
+++ src/game/logic.rs
@@ -1,8 +1,43 @@
-use crate::channels::*;
-use std::thread::{self, JoinHandle};
+use crate::{game::{InputEvent, InputEventHistory, state::State}, channels::*};
+use std::{time::{Duration, Instant}, thread::{self, JoinHandle}, sync::mpsc};
 
-pub fn start_logic_thread(_: WindowToLogicReceiver, _: LogicToPackingSender) -> JoinHandle<()> {
-    thread::spawn(|| {
+const TPS: u32 = 24;
+const SECONDS_PER_TICK: f32 = 1./ (TPS as f32);
+
+pub fn start_logic_thread(window_to_logic_receiver: WindowToLogicReceiver, logic_to_packing_sender: LogicToPackingSender) -> JoinHandle<()> {
+    thread::spawn(move || {
         println!("Running game logic!");
+        let gsm_mutex = logic_to_packing_sender.graphics_state_model;
+        let gsm_channel = logic_to_packing_sender.channel_sender;
+
+        let mut event_history = InputEventHistory::new();
+        let mut state = State::new();
+
+        let mut last_tick = Instant::now();
+        loop {
+            // Handle input events.
+            event_history.handle_inputs(&window_to_logic_receiver.channel_receiver);
+
+            // Run tick.
+            state.tick(event_history.cur_tick_events().expect("This should not be possible"));
+
+            // Update graphics state model.
+            match gsm_mutex.try_lock() {
+                Ok(mut gsm) => {
+                    state.update_graphics_state_model(&mut gsm);
+                    if let Err(error) = gsm_channel.send(Update) {
+                        panic!("Packing thread has deallocated the channel. {}", error);
+                    }
+                },
+                Err(std::sync::TryLockError::Poisoned(error)) => panic!("Graphics state model mutex is poisoned. {}", error),
+                Err(std::sync::TryLockError::WouldBlock) => {},
+            }
+
+            // Wait for next tick if necessary.
+            if last_tick.elapsed().as_secs_f32() < SECONDS_PER_TICK {
+                thread::sleep(Duration::from_secs_f32(SECONDS_PER_TICK - last_tick.elapsed().as_secs_f32()));
+            }
+            last_tick = Instant::now();
+        }
     })
 }
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,9 +2,12 @@ pub mod world;
 
 mod input_event;
 pub use input_event::InputEvent;
+pub use input_event::InputEventHistory;
 
 mod graphics_state_model;
 pub use graphics_state_model::GraphicsStateModel;
 
 mod logic;
 pub use logic::start_logic_thread;
+
+mod state;
