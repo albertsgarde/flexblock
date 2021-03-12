277de7afe5807cb38c6ca4e942e261f35116e1bb
Albert Garde <albertsgarde@gmail.com>
2021-03-12 21:45:31 +0100

    :sparkles: Add player to graphics state model

--- src/game/graphics_state_model.rs
+++ src/game/graphics_state_model.rs
@@ -1,13 +1,15 @@
-use crate::game::world::Terrain;
+use crate::game::{world::Terrain, Player};
 
 pub struct GraphicsStateModel {
     pub terrain: Terrain,
+    pub player: Player
 }
 
 impl GraphicsStateModel {
     pub fn new() -> GraphicsStateModel {
         GraphicsStateModel {
             terrain: Terrain::new(),
+            player: Player::default(),
         }
     }
 }
