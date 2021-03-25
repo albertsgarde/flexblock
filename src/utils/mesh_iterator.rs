f2631a3dfe26e4beabeda1d25e8131e904870e3a
Albert Garde <albertsgarde@gmail.com>
2021-03-25 16:53:21 +0100

    :art: Cargo fmt

--- src/utils/mesh_iterator.rs
+++ src/utils/mesh_iterator.rs
@@ -1,28 +1,34 @@
-use crate::utils::maths::{Zero, One};
+use crate::utils::maths::{One, Zero};
+use glm::{Scalar, TVec3};
 use std::ops::{AddAssign, Neg};
-use glm::{TVec3, Scalar};
 
 /// An iterator that iterates all voxels in a box with the lower corner at origin.
 /// Iterates the x-dimension last.
 /// The upper bound is exclusive.
 pub struct MeshIterator<T>
-    where T: Scalar + PartialOrd + AddAssign + Neg + Zero + One
+where
+    T: Scalar + PartialOrd + AddAssign + Neg + Zero + One,
 {
     cur_loc: TVec3<T>,
     max_loc: TVec3<T>,
 }
 
-impl<T> MeshIterator<T> 
-    where T: Scalar + PartialOrd + AddAssign + Neg<Output = T> + Zero + One
+impl<T> MeshIterator<T>
+where
+    T: Scalar + PartialOrd + AddAssign + Neg<Output = T> + Zero + One,
 {
     /// Creates a MeshIterator for the box with inclusive lower corner at origin and exclusive upper corner at `max_loc`.
     pub fn create(max_loc: TVec3<T>) -> MeshIterator<T> {
-        MeshIterator { cur_loc: TVec3::new(T::zero(), T::zero(), -T::one()), max_loc}
+        MeshIterator {
+            cur_loc: TVec3::new(T::zero(), T::zero(), -T::one()),
+            max_loc,
+        }
     }
 }
 
-impl<T> Iterator for MeshIterator <T>
-    where T: Scalar + PartialOrd + AddAssign + Neg + Zero + One
+impl<T> Iterator for MeshIterator<T>
+where
+    T: Scalar + PartialOrd + AddAssign + Neg + Zero + One,
 {
     type Item = TVec3<T>;
 
@@ -41,45 +47,45 @@ impl<T> Iterator for MeshIterator <T>
 #[cfg(test)]
 mod test {
     use super::*;
-    use glm::{Vec3, IVec3};
-    
+    use glm::{IVec3, Vec3};
+
     #[test]
     fn simple_f32() {
-        let mut iterator = MeshIterator::create(Vec3::new(5.,5.,5.));
+        let mut iterator = MeshIterator::create(Vec3::new(5., 5., 5.));
         assert_eq!(iterator.next(), Some(Vec3::new(0., 0., 0.)));
         for _ in 1..7 {
             iterator.next().unwrap();
         }
-        assert_eq!(iterator.next(), Some(Vec3::new(0.,1.,2.)));
+        assert_eq!(iterator.next(), Some(Vec3::new(0., 1., 2.)));
         for _ in 8..32 {
             iterator.next().unwrap();
         }
-        assert_eq!(iterator.next(), Some(Vec3::new(1.,1.,2.)));
+        assert_eq!(iterator.next(), Some(Vec3::new(1., 1., 2.)));
         for _ in 33..123 {
             iterator.next().unwrap();
         }
-        assert_eq!(iterator.next(), Some(Vec3::new(4.,4.,3.)));
-        assert_eq!(iterator.next(), Some(Vec3::new(4.,4.,4.)));
+        assert_eq!(iterator.next(), Some(Vec3::new(4., 4., 3.)));
+        assert_eq!(iterator.next(), Some(Vec3::new(4., 4., 4.)));
         assert_eq!(iterator.next(), None);
     }
-    
+
     #[test]
     fn simple_i32() {
-        let mut iterator = MeshIterator::create(IVec3::new(5,5,5));
+        let mut iterator = MeshIterator::create(IVec3::new(5, 5, 5));
         assert_eq!(iterator.next(), Some(IVec3::new(0, 0, 0)));
         for _ in 1..7 {
             iterator.next().unwrap();
         }
-        assert_eq!(iterator.next(), Some(IVec3::new(0,1,2)));
+        assert_eq!(iterator.next(), Some(IVec3::new(0, 1, 2)));
         for _ in 8..32 {
             iterator.next().unwrap();
         }
-        assert_eq!(iterator.next(), Some(IVec3::new(1,1,2)));
+        assert_eq!(iterator.next(), Some(IVec3::new(1, 1, 2)));
         for _ in 33..123 {
             iterator.next().unwrap();
         }
-        assert_eq!(iterator.next(), Some(IVec3::new(4,4,3)));
-        assert_eq!(iterator.next(), Some(IVec3::new(4,4,4)));
+        assert_eq!(iterator.next(), Some(IVec3::new(4, 4, 3)));
+        assert_eq!(iterator.next(), Some(IVec3::new(4, 4, 4)));
         assert_eq!(iterator.next(), None);
     }
 }
