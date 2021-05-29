7e86b81d04ff673f1889fa5ee02146dbebf451a9
Albert Garde <albertsgarde@gmail.com>
2021-05-29 10:46:42 +0200

    :sparkles: Delay time specified by module

--- flexblock-synth/src/modules/delay.rs
+++ flexblock-synth/src/modules/delay.rs
@@ -4,12 +4,14 @@ use flexblock_synth_derive::module;
 
 #[module]
 /// Delays the incoming signal by a specified amount of time.
-pub struct Delay<S: Module> {
+pub struct Delay<S: Module, D: Module> {
     buffer: RotatingArray<f32>,
+    sample_rate: u32,
     source: S,
+    delay: D,
 }
 
-impl<S: Module> Delay<S> {
+impl<S: Module, D: Module> Delay<S, D> {
     /// Creates a new Delay module that takes the signal from `source` and delays it `delay_time` seconds.
     ///
     /// # Arguments
@@ -19,21 +21,30 @@ impl<S: Module> Delay<S> {
     /// * `sample_rate` - The used sample rate.
     pub fn new(
         source: ModuleTemplate<S>,
-        delay_time: f32,
+        delay_time: ModuleTemplate<D>,
+        max_delay_time: f32,
         sample_rate: u32,
-    ) -> ModuleTemplate<Delay<S>> {
-        let buffer_size = (delay_time * sample_rate as f32) as usize;
+    ) -> ModuleTemplate<Delay<S, D>> {
+        let buffer_size = (max_delay_time * sample_rate as f32) as usize+1;
         ModuleTemplate {
             module: Delay {
                 buffer: RotatingArray::new(buffer_size, 0.),
+                sample_rate,
                 source: source.module,
+                delay: delay_time.module,
             },
         }
     }
 }
 
-impl<S: Module> Module for Delay<S> {
+impl<S: Module, D: Module> Module for Delay<S, D> {
     fn next(&mut self, sample_num: u64) -> f32 {
-        self.buffer.push_pop(self.source.next(sample_num))
+        let cur_delay_samples = (self.delay.next(sample_num) * self.sample_rate as f32) as usize;
+        if cur_delay_samples >= self.buffer.len() {
+            panic!("Delay must not exceed the maximum delay set at initialization. Max delay samples: {}, cur delay samples: {}", self.buffer.len(), cur_delay_samples);
+        }
+        let source = self.source.next(sample_num);
+        self.buffer.set(cur_delay_samples, |sample| sample + source);
+        self.buffer.push_pop(0.)
     }
 }
