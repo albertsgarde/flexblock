dafda06f86189159abf72e30b1bf6aab7a40ceb0
Albert Garde <albertsgarde@gmail.com>
2021-09-07 18:55:12 +0200

    :sparkles: Made unchecked voxel functions unsafe

--- src/game/world/chunk.rs
+++ src/game/world/chunk.rs
@@ -93,7 +93,7 @@ impl Chunk {
     /// # Arguments
     ///
     /// * `loc` - Will find the type of the voxel at this location in the chunk.
-    pub fn voxel_type_unchecked(&self, loc: ChunkLocation) -> VoxelType {
+    pub unsafe fn voxel_type_unchecked(&self, loc: ChunkLocation) -> VoxelType {
         debug_assert!(loc.index < CHUNK_LENGTH);
         match self {
             Chunk::SingleType(voxel_type) => *voxel_type,
@@ -126,7 +126,7 @@ impl Chunk {
     /// # Arguments
     ///
     /// * `loc` - Will find the type of the voxel at this location in the chunk.
-    pub fn voxel_unchecked(&self, loc: ChunkLocation) -> (VoxelType, Option<&dyn Voxel>) {
+    pub unsafe fn voxel_unchecked(&self, loc: ChunkLocation) -> (VoxelType, Option<&dyn Voxel>) {
         debug_assert!(loc.index < CHUNK_LENGTH);
         match self {
             Chunk::SingleType(voxel_type) => (*voxel_type, None),
@@ -169,7 +169,7 @@ impl Chunk {
         }
     }
 
-    fn ignore_voxel(&self, voxel: ChunkLocation) -> bool {
+    pub unsafe fn ignore_voxel(&self, voxel: ChunkLocation) -> bool {
         raytrace::ignore_voxel_type(self.voxel_type_unchecked(voxel))
     }
 
@@ -192,7 +192,7 @@ impl Chunk {
             let (mut t, mut voxel) =
                 raytrace::voxel_exit(origin, direction, raytrace::round(origin), 1.).unwrap();
             while Chunk::within_chunk(voxel) {
-                if !self.ignore_voxel(voxel.into()) {
+                if !unsafe { self.ignore_voxel(voxel.into()) } {
                     return Some((t, voxel));
                 }
                 let temp = raytrace::voxel_exit(origin, direction, voxel, 1.).unwrap();
