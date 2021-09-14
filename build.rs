35957643fa296c78a59a7660751f98716ee648b4
Albert Garde <albertsgarde@gmail.com>
2021-09-14 16:04:02 +0200

    :poop: Add error messages that won't be printed anyway

--- build.rs
+++ build.rs
@@ -9,8 +9,14 @@ struct BuildConfig {
 impl BuildConfig {
     pub fn load_from_file() -> Self {
         if let Ok(config_string) = fs::read_to_string("build_config.toml") {
-            toml::from_str(&config_string).unwrap_or_default()
+            if let Ok(config) = toml::from_str(&config_string) {
+                config
+            } else {
+                eprintln!("Could not parse build config file.");
+                BuildConfig::default()
+            }
         } else {
+            eprintln!("Could not read build config file.");
             BuildConfig::default()
         }
     }
